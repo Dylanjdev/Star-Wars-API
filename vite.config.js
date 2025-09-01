c49adfc949d454ff4e1c23c294bbf68f24162ca1
Dylan Smith <dylanjsmith.dev@gmail.com>
2025-08-31 20:30:54 -0400

    updated vite.config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/Star-Wars-API/', // Set base for GitHub Pages
   plugins: [react()],
   server: {
     proxy: {
